f85f2ed5b8d5868adb616d0dc72be5ed7166d8ed
maywhale <68489974+maywhale@users.noreply.github.com>
2024-07-29 20:43:32 -0600

    Added multifile scanning and did a little bit of refactoring (also cool banner Added)

--- Melinoe.py
+++ Melinoe.py
@@ -1,4 +1,4 @@
-import yara, time
+import yara, time, os
 
 class Scanner:
 
@@ -23,34 +23,71 @@ class Scanner:
             print('exiting....')
             exit()
     
-    def scan_target(self):
 
-        print('starting file or directory scan\n')
+
+
+    def scan_target(self):
+        if (os.path.isfile(self.target_path)):
+            self.scan_single(self.target_path)
+        else:
+            self.scan_directory()
+
+        
+    def scan_single(self, target):
 
         # error catching
         
         try:
-            matches = self.rules.match(self.target_path)
+            matches = self.rules.match(target)
 
         except:
             print('invalid file path')
 
         for item in matches:
-            print(f'MAY CONTAIN MALWARE: "{self.target_path}" Contains {item}')
+            print(f'MAY CONTAIN MALWARE: "{target}" Contains {item}')
+
+    
+    def scan_directory(self):
         
-
-
+        # https://stackoverflow.com/questions/16953842/using-os-walk-to-recursively-traverse-directories-in-python
+        for root, dirs, files in os.walk(self.target_path):
+            path = root.split(os.sep)
+            # print((len(path) - 1) * '---', os.path.basename(root))
+            for file in files:
+               # print(len(path) * '---', file)
+                full_path = os.path.join(root,file)
+                self.scan_single(full_path)
+                
 
 def print_banner():
-    pass
+
+    banner = r"""
+     ______   _______  _______ _________           _________ _______    _______  _______  _______  _        _______  _______ 
+    (  __  \ (  ____ \(  ___  )\__   __/|\     /|  \__   __/(  ___  )  (  ____ \(  ____ )(  ___  )( (    /|(  ___  )(  ____ \
+    | (  \  )| (    \/| (   ) |   ) (   | )   ( |     ) (   | (   ) |  | (    \/| (    )|| (   ) ||  \  ( || (   ) || (    \/
+    | |   ) || (__    | (___) |   | |   | (___) |     | |   | |   | |  | |      | (____)|| |   | ||   \ | || |   | || (_____ 
+    | |   | ||  __)   |  ___  |   | |   |  ___  |     | |   | |   | |  | |      |     __)| |   | || (\ \) || |   | |(_____  )
+    | |   ) || (      | (   ) |   | |   | (   ) |     | |   | |   | |  | |      | (\ (   | |   | || | \   || |   | |      ) |
+    | (__/  )| (____/\| )   ( |   | |   | )   ( |     | |   | (___) |  | (____/\| ) \ \__| (___) || )  \  || (___) |/\____) |
+    (______/ (_______/|/     \|   )_(   |/     \|     )_(   (_______)  (_______/|/   \__/(_______)|/    )_)(_______)\_______)
+                                                                                                                         
+    """
+
+    print(banner)
+
+    time.sleep(1)
+
+
 
 def get_input():
 
-    yara_path = input('Enter the path of the yara file: \n')
+    yara_path = input('Enter the path of your yara file: \n')
     file_path = input('Enter the path to the directory or file: \n')
     return (yara_path,file_path)
 
 def main():
+
+    print_banner()
 
     yara_path,file_path = get_input()
 
@@ -62,10 +99,10 @@ def main():
 
     scanner.scan_target()
         
-    #rules = yara.compile(filepath='injection_rules.yar')
-    #matches = rules.match('d:\coding projects\malware development\win32api learning\output\process_injection_OHMYGAHHH.exe')
+    # rules = yara.compile(filepath='injection_rules.yar')
+    # matches = rules.match('d:\coding projects\malware development\win32api learning\output')
     #Nt_check = rules.match('D:\coding projects\maldev crow\TAPI Injection\output\main.exe')
-    #print(f"win32 api check {matches}")
+    # print(f"win32 api check {matches}")
     #print(f"NT check {Nt_check}")
 
 if __name__ == '__main__':
